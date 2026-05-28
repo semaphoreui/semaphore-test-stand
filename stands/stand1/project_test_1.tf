0b91ded59c06544b39df43a633665f756a041529
Denis Gukov <denguk@gmail.com>
2026-05-28 14:47:17 +0500

    feat: tf code for test

--- stands/stand1/project_test_1.tf
+++ stands/stand1/project_test_1.tf
@@ -1,3 +1,105 @@
 resource "semaphoreui_project" "test_1" {
   name = "Test 1"
 }
+
+resource "semaphoreui_project_key" "semaphore_test_stand" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "semaphore_test_stand"
+
+  ssh = {
+    private_key = file("${path.module}/../../semaphore_test_stand")
+    passphrase  = "123456"
+  }
+}
+
+resource "semaphoreui_project_repository" "semaphore_test_stand" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "semaphore-test-stand"
+  url        = "git@github.com:semaphoreui/semaphore-test-stand.git"
+  branch     = "main"
+  ssh_key_id = semaphoreui_project_key.semaphore_test_stand.id
+}
+
+resource "semaphoreui_project_key" "none" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "none"
+  none       = {}
+}
+
+resource "semaphoreui_project_key" "vault_default" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "vault_default"
+
+  login_password = {
+    login    = ""
+    password = "defaultpass"
+  }
+}
+
+resource "semaphoreui_project_key" "vault_1" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "vault_1"
+
+  login_password = {
+    login    = ""
+    password = "pass1"
+  }
+}
+
+resource "semaphoreui_project_key" "vault_2" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "vault_2"
+
+  login_password = {
+    login    = ""
+    password = "pass2"
+  }
+}
+
+resource "semaphoreui_project_inventory" "localhost" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "localhost"
+  ssh_key_id = semaphoreui_project_key.none.id
+
+  static = {
+    inventory = <<-EOT
+      [local]
+      localhost ansible_connection=local
+    EOT
+  }
+}
+
+resource "semaphoreui_project_environment" "default" {
+  project_id = semaphoreui_project.test_1.id
+  name       = "default"
+}
+
+resource "semaphoreui_project_template" "multiple_vault_passwords" {
+  project_id     = semaphoreui_project.test_1.id
+  environment_id = semaphoreui_project_environment.default.id
+  inventory_id   = semaphoreui_project_inventory.localhost.id
+  repository_id  = semaphoreui_project_repository.semaphore_test_stand.id
+  name           = "multiple-vault-passwords"
+  playbook       = "tests/multiple-vault-passwords/test.yml"
+
+  vaults = [
+    {
+      name = ""
+      password = {
+        vault_key_id = semaphoreui_project_key.vault_default.id
+      }
+    },
+    {
+      name = "vault1"
+      password = {
+        vault_key_id = semaphoreui_project_key.vault_1.id
+      }
+    },
+    {
+      name = "vault2"
+      password = {
+        vault_key_id = semaphoreui_project_key.vault_2.id
+      }
+    },
+  ]
+}
