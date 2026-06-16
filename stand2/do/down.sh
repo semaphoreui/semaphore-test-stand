5f9cc636f13e1f475047d807e8b22d4567df659b
Denis Gukov <denguk@gmail.com>
2026-06-16 23:23:22 +0500

    feat: install docker to runner

--- stand2/do/down.sh
+++ stand2/do/down.sh
@@ -3,11 +3,13 @@ set -a; source ../.env; set +a
 cd runner
 
 terraform workspace select $1
-$TARGETS=$(terraform state list | grep -v -E "digitalocean_certificate|digitalocean_domain|cloudflare_record" | awk '{print "-target=" $0}')
-terraform destroy $TARGETS -auto-approve
+
+terraform destroy -auto-approve
 
 cd ../server
 
 terraform workspace select $1
 
-terraform destroy -auto-approve
+$TARGETS=$(terraform state list | grep -v -E "digitalocean_certificate|digitalocean_domain|cloudflare_record" | awk '{print "-target=" $0}')
+
+terraform destroy $TARGETS -auto-approve
