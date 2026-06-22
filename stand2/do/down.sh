a0542999b4220c98069ec2e4e5ad7e6b45de0fbe
Denis Gukov <denguk@gmail.com>
2026-06-22 11:39:41 +0500

    chore: stop server

--- stand2/do/down.sh
+++ stand2/do/down.sh
@@ -11,26 +11,23 @@ terraform destroy -auto-approve
 
 cd ..
 
+cd server
 
+if [[ -n "$1" ]]; then
+  terraform workspace select $1
+fi
 
+TARGETS=("${(@f)$(terraform state list |
+  grep -v -E 'cloudflare_zone|digitalocean_certificate|digitalocean_domain|cloudflare_record' |
+  sed 's/^/-target=/'
+)}")
 
-# cd server
+if (( ${#TARGETS[@]} == 0 )); then
+  echo "No resources selected"
+  exit 1
+fi
 
-# if [[ -n "$1" ]]; then
-#   terraform workspace select $1
-# fi
+echo "Targets:"
+printf '  %s\n' "${TARGETS[@]}"
 
-# TARGETS=("${(@f)$(terraform state list |
-#   grep -v -E 'cloudflare_zone|digitalocean_certificate|digitalocean_domain|cloudflare_record' |
-#   sed 's/^/-target=/'
-# )}")
-
-# if (( ${#TARGETS[@]} == 0 )); then
-#   echo "No resources selected"
-#   exit 1
-# fi
-
-# echo "Targets:"
-# printf '  %s\n' "${TARGETS[@]}"
-
-# terraform destroy "${TARGETS[@]}" -auto-approve
+terraform destroy "${TARGETS[@]}" -auto-approve
